4ed184fb20fe976daff8337eb35bc05a06f36bc6
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-21 03:44:27 +1000

    Update program to use functions

--- celstofahrtable.c
+++ celstofahrtable.c
@@ -1,15 +1,18 @@
 /*
     celstofahrtable.c
     Author: Brendan Gasparin
-    Date: 14 April 2024
+    Date: 21 April 2024
     Prints a table of Celcius to Fahrenheit values.
 */
 
 #include <stdio.h>
 
+float celstofahr(float cels);
+void printbanner();
+
 int main()
 {
-    float celsius, fahrenheit;
+    float celsius;
     int lower, upper, step;
 
     lower = 0;      // lower bound of Celsius in table
@@ -18,13 +21,25 @@ int main()
     celsius = (float) lower;
 
     // print banner
-    printf("***************************\n");
-    printf("CELSIUS TO FAHRENHEIT TABLE\n");
-    printf("***************************\n\n");
+    printbanner();
 
     while (celsius <= upper) {
-        fahrenheit = celsius * 9.0 / 5.0 + 32.0;
-        printf("%3.0f %17.0f\n", celsius, fahrenheit);
+        printf("%7.0f %13.0f\n", celsius, celstofahr(celsius));
         celsius += step;
     }
+
+    return 0;
+}
+
+// converts float cels parameter and returns fahrenheit value
+float celstofahr(float cels)
+{
+    return cels * 9.0 / 5.0 + 32.0;
+}
+
+void printbanner()
+{
+    printf("***************************\n");
+    printf("CELSIUS TO FAHRENHEIT TABLE\n");
+    printf("***************************\n");
 }
